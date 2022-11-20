9ef0c25d2427cd6e15aee9db3f7f0bcb9aafa97b
AjaxVM <roebros@gmail.com>
2022-11-20 09:35:44 -0600

    trying a new config to make vercel run

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,8 +6,6 @@ const app = express()
 
 app.use(v1)
 
-export default (): any => {
-  app.listen(3000, () => {
-    console.log('API running at http://localhost:3000')
-  })
-}
+app.listen(3000, () => {
+  console.log('API running at http://localhost:3000')
+})
